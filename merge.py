f0169e364e11923ccb00fb794516da27890f2749
ldm2060 <ldm200026@live.cn>
2025-04-14 20:28:09 +0800

    Add asynchronous image downloader and update README with logo usage instructions
    - Implemented `merge.py` to download channel images asynchronously using aiohttp and tqdm for progress tracking.
    - Added a function to transform traditional Chinese to simplified Chinese using OpenCC.
    - Updated README.md with links to access logos from GitHub and Gitee.

---
+++ merge.py
@@ -0,0 +1,72 @@
+import xml.etree.ElementTree as ET
+from collections import defaultdict
+import aiohttp
+import asyncio
+from tqdm.asyncio import tqdm_asyncio  # 引入 tqdm 的异步支持
+from datetime import datetime
+import gzip
+import shutil
+from xml.dom import minidom
+import re
+from opencc import OpenCC
+import os
+from tqdm import tqdm  # 引入 tqdm 的同步支持
+
+def transform2_zh_hans(string):
+    cc = OpenCC("t2s")
+    new_str = cc.convert(string)
+    return new_str
+
+
+def get_urls():
+    urls = []
+    with open('config.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                urls.append(line)
+    return urls
+
+
+async def process_channel(channel, urls, semaphore):
+    if (os.exists(f'img/{channel}.png')):
+        return
+    flag = False
+    async with semaphore:  # 使用信号量限制并发
+        for url in urls:
+            if flag:
+                break
+            now_url = url +"/"+ channel + '.png'
+            async with aiohttp.ClientSession() as session:
+                async with session.get(now_url) as response:
+                    if response.status == 200:
+                        content = await response.read()
+                        with open(f'images/{channel}.png', 'wb') as f:
+                            f.write(content)
+                        flag = True
+
+channels_url = "https://github.com/mytv-android/iptv-api/raw/refs/heads/master/config/demo.txt"
+async def main():
+    urls = get_urls()
+    channels = []
+    async with aiohttp.ClientSession() as session:
+        async with session.get(channels_url) as response:
+            if response.status == 200:
+                content = await response.text()
+                channels += [line for line in content.split(
+                    '\n') if line and not line.startswith('#')]
+    channels = list(set(channels))  # 去重
+    semaphore = asyncio.Semaphore(10)  # 限制并发任务数量为 10
+
+    tasks = []
+    for channel in channels:
+        tasks.append(process_channel(channel, urls, semaphore))
+    with tqdm_asyncio(total=len(tasks), desc="Downloading channels") as pbar:
+        for task in asyncio.as_completed(tasks):
+            await task
+            pbar.update(1)
+    print("All channels Processed.")                        
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,10 @@
+# 集众家所长的logo
+
+## 使用
+https://github.com/mytv-android/myTVlogo/raw/refs/heads/master/output/*.png
+https://github.com/mytv-android/myTVlogo/raw/refs/heads/master/output/*.png
+
+或者
+
+https://gitee.com/mytv-android/myTVlogo/raw/master/output/*.png
+https://gitee.com/mytv-android/myTVlogo/raw/master/output/*.png
