934223ad79c85fbb8ec523907af236f9a5baaf98
ldm2060 <ldm200026@live.cn>
2025-07-02 22:15:50 +0800

    更新频道和配置

--- merge.py
+++ merge.py
@@ -12,6 +12,10 @@ from opencc import OpenCC
 import os
 from tqdm import tqdm  # 引入 tqdm 的同步支持
 
+retry = 3  # 重试次数
+sleep_time = 5  # 重试间隔时间（秒）
+max_tasks = 10  # 最大并发任务数
+
 def transform2_zh_hans(string):
     cc = OpenCC("t2s")
     new_str = cc.convert(string)
@@ -37,17 +41,26 @@ async def process_channel(channel, urls, semaphore):
             if flag:
                 break
             now_url = url +"/"+ channel + '.png'
-            async with aiohttp.ClientSession() as session:
-                async with session.get(now_url) as response:
-                    if response.status == 200:
-                        content = await response.read()
-                        with open(f'./img/{channel}.png', 'wb') as f:
-                            f.write(content)
-                        flag = True
+            retry_count = 0
+            while retry_count < retry and not flag:
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        async with session.get(now_url) as response:
+                            if response.status == 200:
+                                content = await response.read()
+                                with open(f'./img/{channel}.png', 'wb') as f:
+                                    f.write(content)
+                                flag = True
+                            retry_count = retry
+                except Exception as e:
+                    retry_count += 1
+                    print(f"Error fetching {now_url}: {e}")
+                    await asyncio.sleep(sleep_time)  # 等待一段时间后重试
 
 channels_url = [
     "https://github.com/mytv-android/iptv-api/raw/refs/heads/master/config/demo.txt", 
-    "https://github.com/mytv-android/myTVlogo/raw/refs/heads/main/channel.txt"
+    "https://github.com/mytv-android/myTVlogo/raw/refs/heads/main/channel.txt",
+    "https://github.com/ldm0206/iptv-api/raw/refs/heads/master/config/demo.txt"
 ]
 async def main():
     urls = get_urls()
@@ -60,7 +73,7 @@ async def main():
                     channels += [line for line in content.split(
                         '\n') if line and not line.startswith('#')]
     channels = list(set(channels))  # 去重
-    semaphore = asyncio.Semaphore(10)  # 限制并发任务数量为 10
+    semaphore = asyncio.Semaphore(max_tasks)  # 限制并发任务数量为 10
 
     tasks = []
     for channel in channels:
